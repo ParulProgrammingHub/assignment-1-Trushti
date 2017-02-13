515b7279de8054a4f1d46d416c3672ae6f9ec2a5
Trushti <150303105153@paruluniversity.ac.in>
2017-02-13 20:13:07 +0530

    Create 153Q2.py

---
+++ 153Q2.py
@@ -0,0 +1,6 @@
+PI=3.14
+radious=float(input("Enter the radious of a circle:"))
+area=PI*radious*radious
+circumference=2*PI*radious
+print("Area of a circle=%.2f"%area)
+print("circumfarance of a circle=%.2f"%circumference)
